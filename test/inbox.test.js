123d71173bdc3ba23b2ce859171f555a032a74d6
SHEFFmachine <sheff@sheffmachine.com>
2018-07-07 20:38:41 -0700

    Write tests around the inbox contract

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,6 +1,34 @@
+const { interface, bytecode } = require('../compile')
+
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 
+let accounts, inbox
+const INITIAL_MESSAGE = 'Hi there!'
 
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts()
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+    .send({ from: accounts[0], gas: '1000000' })
+})
+
+describe('Inbox', () => {
+  it('shoud deploy a contract', () => {
+    assert.ok(inbox.options.address)
+  })
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, INITIAL_MESSAGE)
+  })
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] })
+
+    const message = await inbox.methods.message().call()
+    assert.equal(message, 'bye')
+  })
+})
